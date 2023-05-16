57dfeee0f27a245aa99973433e8c29622ae5f3b8
tufstraka <keithkadima@gmail.com>
2023-05-16 10:09:55 +0300

    Update styles.js

--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   paper: {
-    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', borderRadius: '30px', width: '100px',
+    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', borderRadius: '10px', width: '100px',
   },
   mapContainer: {
     height: '85vh', width: '100%',
