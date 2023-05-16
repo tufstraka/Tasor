a712007b1d8c603c2550275ffd77678fb675ec1b
tufstraka <keithkadima@gmail.com>
2023-05-16 10:21:29 +0300

    Update Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab/Rating';
 
 import mapStyles from '../../mapStyles';
 import useStyles from './styles.js';
